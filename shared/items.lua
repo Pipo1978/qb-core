c47a76dab51919ae407d360ca949903ee3bbc302
Pipo <pipo.cool@gmail.com>
2024-02-20 14:43:58 -0300

    Modificações efetuadas em: items.lua

--- shared/items.lua
+++ shared/items.lua
@@ -382,4 +382,6 @@ QBShared.Items = {
     newscam                      = { name = 'newscam', label = 'News Camera', weight = 100, type = 'item', image = 'newscam.png', unique = true, useable = true, shouldClose = true, combinable = nil, description = 'A camera for the news' },
     newsmic                      = { name = 'newsmic', label = 'News Microphone', weight = 100, type = 'item', image = 'newsmic.png', unique = true, useable = true, shouldClose = true, combinable = nil, description = 'A microphone for the news' },
     newsbmic                     = { name = 'newsbmic', label = 'Boom Microphone', weight = 100, type = 'item', image = 'newsbmic.png', unique = true, useable = true, shouldClose = true, combinable = nil, description = 'A Useable BoomMic' },
+
+    racing_gps                   = {name = 'racing_gps', label = 'Racing GPS', weight = 500, type = 'item', image = 'racing_gps.png', unique = true, useable = true, shouldClose = true, description = 'Wroom wroom.'},
 }
